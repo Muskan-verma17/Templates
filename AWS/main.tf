3902d2351359921b94ec26e23db6e9ea4388f9ff
Muskan Verma <muskanverma@Muskans-MacBook-Pro.local>
2023-01-31 17:15:58 +0530

    added aws vpc

---
+++ AWS/main.tf
@@ -0,0 +1,14 @@
+
+provider "aws" {
+  profile = "default"
+  region = "eu-west-1"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "main"
+  }
+}
